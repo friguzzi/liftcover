ead3562b40ff9da160418c9158ab4cf1b5ffa198
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2023-01-11 17:33:41 +0100

    added regularization

--- prolog/liftcover.pl
+++ prolog/liftcover.pl
@@ -121,6 +121,13 @@ default_setting_lift(approx_value,100).
 default_setting_lift(zero,0.01).
 default_setting_lift(minus_infinity,-1.0e20).
 
+default_setting_lift(regularization,1). % regularization: 0-no regularization, l1, l2, bayeisan and no otherwise
+default_setting_lift(gamma,10). % set the value of gamma for regularization l1 and l2
+default_setting_lift(ab,[0,10]). % set the value of a and b for regularization baysian
+default_setting_lift(min_probability,1e-5).  % Threshold of the probability under which the clause is dropped
+
+
+
 /**
  * induce_lift(+TrainFolds:list_of_atoms,-P:probabilistic_program) is det
  *
@@ -283,20 +290,28 @@ to_dyn(M,P/A):-
 
 
 
-learn_struct(Pos,Neg,Mod,Beam,R2,Score):-  %+Beam:initial theory of the form [rule(NR,[h],[b]],...], -R:final theory of the same form, -CLL
+learn_struct(Pos,Neg,Mod,Beam,R,Score):-  %+Beam:initial theory of the form [rule(NR,[h],[b]],...], -R:final theory of the same form, -CLL
   format2(Mod,"Clause search~n~n",[]),
   Mod:local_setting(max_iter,M),
   Mod:set_lift(depth_bound,false),
   cycle_beam(Beam,Mod,Pos,Neg,[],CL,[],_BG,M),
   maplist(get_cl,CL,LC),
-  learn_param(LC,Mod,Pos,Neg,R2,Score),
+  learn_param(LC,Mod,Pos,Neg,R1,Score),
 /*  CL=[(C0,S)|RCL],
   format2("Theory search~n~n",[]),
   Mod:local_setting(max_iter_structure,MS),
   cycle_structure(RCL,Mod,[C0],S,-1e20,Pos,Neg,R2,Score,MS),
 */
+  (Mod:local_setting(regularization,0)->
+    R=R1
+  ;
+    Mod:local_setting(min_probability,Min_prob),
+    remove_clauses(R1,Min_prob,R,Num)
+  ),
+  length(R1,NumBR),
+  format2(Mod,"Rules before regularization: ~d~nAfter regularization ~d~n ",[NumBR,Num]),
   format2(Mod,"Best target theory~n~n",[]),
-  write_rules2(Mod,R2,user_output).
+  write_rules2(Mod,R,user_output).
 
 get_cl((C,_),C).
 
@@ -532,7 +547,15 @@ induce_parameters(M:Folds,R):-
   process_clauses(R00,M,R0),
   statistics(walltime,[_,_]),
   find_ex(DB,M,Pos,Neg,_NPos,_NNeg),
-  learn_param(R0,M,Pos,Neg,R,Score),
+  learn_param(R0,M,Pos,Neg,R1,Score),
+  (M:local_setting(regularization,0)->
+    R=R1
+  ;
+    M:local_setting(min_probability,Min_prob),
+    remove_clauses(R1,Min_prob,R,Num)
+  ),
+  length(R1,NumBR),
+  format2(M,"Rules before regularization: ~d~nAfter regularization ~d~n ",[NumBR,Num]),
   statistics(walltime,[_,CT]),
   CTS is CT/1000,
   format2(M,'/* EMBLEM Final score ~f~n',[Score]),
@@ -544,6 +567,19 @@ induce_parameters(M:Folds,R):-
     true
   ).
 
+remove_clauses(Rules,Prob,RulesOut,Num):-
+  remove_clauses_loop(Rules,Prob,0,Num,[],RulesOut).
+
+remove_clauses_loop([],_,Num,Num,Rules,Rules):-!.
+remove_clauses_loop([Rule|Rest],Prob,NumCur,Num,RulesCur,RulesOut):-
+  Rule=rule(_N,[_Head:Par|_],_,_),
+  (Par < Prob ->
+    NumCur1 is NumCur+1,
+    remove_clauses_loop(Rest, Prob, NumCur1,Num,RulesCur, RulesOut)
+    ;
+    append(RulesCur,[Rule],RulesCurNew),
+    remove_clauses_loop(Rest, Prob, NumCur,Num,RulesCurNew, RulesOut)
+  ).
 /**
  * induce_par(+TrainFolds:list_of_atoms,+TestFolds:list_of_atoms,-P:probabilistic_program,-LL:float,-AUCROC:float,-ROC:dict,-AUCPR:float,-PR:dict) is det
  *
@@ -701,13 +737,38 @@ maximization_quick(Eta,M,Par):-
   maplist(maximize(M),Eta,Par).
 
 maximize(M,[Eta0,Eta1],Par):-
-  M:local_setting(zero,Zero),
-  (Eta0==0.0,Eta1==0.0 ->
-   Par is Eta1/Zero
-   % or simply Par is 0.0
-   ;
-   Par is Eta1/(Eta0+Eta1)
+  (M:local_setting(regularization,l1)->
+    maximize_L1(M,[Eta0,Eta1],Par)
+  ;
+    (M:local_setting(regularization,l2)->
+        maximize_L2(M,[Eta0,Eta1],Par)
+    ;
+      (M:local_setting(regularization,bayesian)->
+        maximize_bayesian(M,[Eta0,Eta1],Par)
+      ;
+        (Eta0+Eta1=:=0.0->
+          Par=0.0
+        ;
+          Par is Eta1/(Eta0+Eta1)  
+        )
+      )
+    )
   ).
+
+
+maximize_L1(M,[Eta0,Eta1],Par):-
+  M:local_setting(gamma,Gamma),
+  Par is 4*Eta1/(2*(Gamma+Eta0+Eta1+sqrt((Eta0+Eta1)^2+Gamma^2+2*Gamma*(Eta0-Eta1)))).
+
+maximize_L2(M,[Eta0,Eta1],Par):-
+  M:local_setting(gamma,Gamma),
+  Sum is 3*Eta0+3*Eta1+Gamma,
+  Arccos is acos(sqrt(Gamma/Sum)*(9*Eta0/2-9*Eta1+Gamma)),
+  Par is 2*sqrt(Sum/Gamma)*cos(Arccos/3-2*pi/3)/3+1/3.
+
+maximize_bayesian(M,[Eta0,Eta1],Par):-
+  M:local_setting(ab,[A,B]),
+  Par is (Eta1+A)/(Eta0+Eta1+A+B).
 
 /*....scan_pos predicate..... */
 
